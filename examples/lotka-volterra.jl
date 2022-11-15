a36802376b3ededfe2f6377fd7860e0fbcbdc8a6
Ilia <i.ilmer@icloud.com>
2022-11-15 10:32:33 -0500

    test and example dirs
    Signed-off-by: Ilia <i.ilmer@icloud.com>

---
+++ examples/lotka-volterra.jl
@@ -0,0 +1,17 @@
+using ModelingToolkit
+using DifferentialEquations, Plots
+
+@parameters k1 k2 k3
+@variables t r(t) w(t)
+D = Differential(t)
+
+u0 = [100.0, 100.0]
+tspan = (0.0, 10.0)
+datasize = 100
+tsteps = range(tspan[1], tspan[2], length=datasize)
+p_true = [0.02, 0.03, 0.05] # True Parameters
+
+@named lv = ODESystem([D(r) ~ k1 * r - k2 * r * w, D(w) ~ -k3 * w + k2 * r * w])
+
+prob_true = ODEProblem(lv, u0, tspan, p_true)
+solution_true = solve(prob_true, Tsit5(), p=p_true, saveat=tsteps)
