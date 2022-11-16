a012724fbb0e222f9fb925aed921f9978aa49cfb
Ilia <i.ilmer@icloud.com>
2022-11-16 14:25:34 -0500

    add new imports
    Signed-off-by: Ilia <i.ilmer@icloud.com>

--- src/ParameterEstimation.jl
+++ src/ParameterEstimation.jl
@@ -4,19 +4,24 @@ import ModelingToolkit
 import ModelingToolkit: substitute
 
 import LinearSolve
+import LinearSolve: LinearProblem, solve
 import SIAN
 
 import Nemo
 import Nemo: fmpq_mpoly, total_degree, vars, var_index, evaluate
+import HomotopyContinuation
 
 import Groebner
 import Groebner: groebner
 
 import LinearAlgebra
 
-include("rational_interpolation.jl")
+include("rational_interpolation/rational_interpolation.jl")
+include("rational_interpolation/construct_equations.jl")
+
 include("identifiability/get_identifiability.jl")
 
+include("utils.jl")
 export get_identifiability
 
 end
