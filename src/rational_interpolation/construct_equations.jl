097ab8f03a8ebc02acc7c71e12668f190165757c
Ilia <i.ilmer@icloud.com>
2022-11-16 14:44:17 -0500

    add rational interpolation code and system processing code
    Signed-off-by: Ilia <i.ilmer@icloud.com>

---
+++ src/rational_interpolation/construct_equations.jl
@@ -0,0 +1,24 @@
+function differentiate_interpolated(interpolated_function, diff_order::Int)
+    τ = Taylor1(diff_order + 1)
+    taylor_expation = interpolated_function(τ)
+    return taylor_expantion
+end
+
+function construct_poly_equations(identifiability_result::Dict, interpolated_function, diff_order::Int)
+    taylor_expantion = differentiate_interpolated(interpolated_function, diff_order)
+    Et = identifiability_result["Et"]
+    y_eq = OrderedDict()
+    for (key, value) in pairs(identifiability_result["Y_eq"])
+        println(value)
+        y_eq[HomotopyContinuation.variables(Symbol(key))[1]] = taylor_expantion[Int(value)]
+    end
+
+    Et_hc = [HomotopyContinuation.subs(ParameterEstimation.nemo2hc(each), pairs(y_eq)...) for each in Et]
+    return Et_hc
+end
+
+function squarify_system(poly_system::Vector{Expression})
+    indets = HomotopyContinuation.variables(poly_system)
+    M = randn(1, length(poly_system) - length(indets) + 1)
+    return vcat(poly_system[1:length(indets)-1], M * poly_system[length(indets):end])
+end
---
+++ src/rational_interpolation/rational_interpolation.jl
@@ -0,0 +1,31 @@
+function rational_interpolation_coefficients(x, y, n)
+    # x and y are vectors of length N
+    # n is numerator degree
+    # returns the coefficients of the numerator and denominator polynomials
+    N = length(x)
+    m = N - n + 1
+    A = zeros(N, N)
+    if m > 0
+        A_left_submatrix = reduce(hcat, [x .^ (i - 1) for i in 1:n])
+        A_right_submatrix = reduce(hcat, [x .^ (i - 1) for i in 1:m-1])
+        A = hcat(A_left_submatrix, -y .* A_right_submatrix)
+        b = y .* (x .^ (m - 1))
+        prob = LinearSolve.LinearProblem(A, b)
+        c = LinearSolve.solve(prob)
+        return c[1:n], [c[n+1:end]; 1]
+    else
+        A = reduce(hcat, [x .^ i for i in 0:n])
+        b = y
+        prob = LinearSolve.LinearProblem(A, b)
+        c = LinearSolve.solve(prob)
+        return c, [1]
+    end
+end
+
+function interpolate(time, sample, numer_degree::Int)
+    numer_coef, denom_coef = rational_interpolation_coefficients(time, sample, numer_degree + 1)
+    numer_function(t) = sum(numer_coef[i] * t^(i - 1) for i in 1:length(numer_coef))
+    denom_function(t) = sum(denom_coef[i] * t^(i - 1) for i in 1:length(denom_coef))
+    interpolated_function(t) = numer_function(t) / denom_function(t)
+    return interpolated_function
+end
