f1dc92a26ef4995e05a264406561517f745836f6
Ilia <i.ilmer@icloud.com>
2022-11-22 20:15:15 -0500

    examples
    Signed-off-by: Ilia <i.ilmer@icloud.com>

--- examples/lotka-volterra.jl
+++ examples/lotka-volterra.jl
@@ -2,7 +2,6 @@ import ParameterEstimation
 
 using ModelingToolkit, DifferentialEquations, Plots
 using Nemo, HomotopyContinuation
-using SIAN: get_order_var
 using TaylorSeries
 
 @parameters k1 k2 k3
@@ -10,23 +9,20 @@ using TaylorSeries
 D = Differential(t)
 
 u0 = [100.0, 100.0]
-tspan = (0.0, 1.0)
-datasize = 20
-tsteps = range(tspan[1], tspan[2], length = datasize)
-p_true = [0.02, 0.03, 0.05] # True Parameters
-measured_data = [y1 ~ r]
+time_interval = (0.0, 1.0)
+datasize = 24
+tsteps = range(time_interval[1], time_interval[2], length = datasize)
+p_true = [0.03, 0.02, 0.05] # True Parameters
+measured_quantities = [y1 ~ r]
 
-@named lv = ODESystem([D(r) ~ k1 * r - k2 * r * w, D(w) ~ -k3 * w + k2 * r * w])
+@named model = ODESystem([D(r) ~ k1 * r - k2 * r * w, D(w) ~ -k3 * w + k2 * r * w])
 
-prob_true = ODEProblem(lv, u0, tspan, p_true)
+prob_true = ODEProblem(model, u0, time_interval, p_true)
 solution_true = ModelingToolkit.solve(prob_true, Tsit5(), p = p_true, saveat = tsteps)
-r_sample = solution_true[1, :]
-g = ParameterEstimation.interpolate(tsteps, r_sample, Int(datasize / 2))
-err = maximum(abs.(r_sample - g.(tsteps)))
-print("Error in interpolation: ", err)
-diff_order = length(p_true) + length(u0)
+data_sample = solution_true[1, :]
 
-τ = Taylor1(diff_order + 1)
-g_expanded = g(τ)
-
-id_result = ParameterEstimation.get_identifiability(lv; measured_quantities = measured_data)
+interpolation_degree = 18
+results = ParameterEstimation.estimate(model, measured_quantities, data_sample,
+                                       time_interval,
+                                       interpolation_degree)
+print(results)
