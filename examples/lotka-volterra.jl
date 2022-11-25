a12ace345d80104aa05029f242ec9710f334e75b
Ilia <i.ilmer@icloud.com>
2022-11-25 09:49:58 -0500

    small changes to the example
    Signed-off-by: Ilia <i.ilmer@icloud.com>

--- examples/lotka-volterra.jl
+++ examples/lotka-volterra.jl
@@ -10,10 +10,12 @@ D = Differential(t)
 
 u0 = [100.0, 100.0]
 time_interval = (0.0, 1.0)
-datasize = 24
+datasize = 15
 tsteps = range(time_interval[1], time_interval[2], length = datasize)
 p_true = [0.03, 0.02, 0.05] # True Parameters
 measured_quantities = [y1 ~ r]
+states = [r, w]
+parameters = [k1, k2, k3]
 
 @named model = ODESystem([D(r) ~ k1 * r - k2 * r * w, D(w) ~ -k3 * w + k2 * r * w])
 
@@ -21,8 +23,10 @@ prob_true = ODEProblem(model, u0, time_interval, p_true)
 solution_true = ModelingToolkit.solve(prob_true, Tsit5(), p = p_true, saveat = tsteps)
 data_sample = solution_true[1, :]
 
-interpolation_degree = 18
+interpolation_degree = 7
 results = ParameterEstimation.estimate(model, measured_quantities, data_sample,
                                        time_interval,
                                        interpolation_degree)
 print(results)
+best_result = ParameterEstimation.filter_solutions(results, model, time_interval,
+                                                   data_sample)
