d75513e736081dcade76c4368beb364092a7cc35
Ilia <i.ilmer@icloud.com>
2022-11-27 13:41:35 -0500

    example with iteration over degrees
    Signed-off-by: Ilia <i.ilmer@icloud.com>

--- examples/fitzhugh-nagumo.jl
+++ examples/fitzhugh-nagumo.jl
@@ -1,6 +1,6 @@
 import ParameterEstimation
 
-using ModelingToolkit, Plots, Nemo, HomotopyContinuation
+using ModelingToolkit, Plots, Nemo, HomotopyContinuation, DifferentialEquations
 
 @parameters g a b
 @variables t V(t) R(t) y1(t)
@@ -8,7 +8,7 @@ D = Differential(t)
 
 u0 = [1.0, -1.0]
 time_interval = (0.0, 1.2)
-datasize = 16
+datasize = 20
 tsteps = range(time_interval[1], time_interval[2], length = datasize)
 p_true = [2, 2 / 10, 2 / 10] # True Parameters
 measured_quantities = [y1 ~ V]
@@ -25,9 +25,5 @@ solution_true = ModelingToolkit.solve(prob_true, Tsit5(), p = p_true, saveat = t
 data_sample = solution_true[1, :]
 
 interpolation_degree = 7
-results = ParameterEstimation.estimate(model, measured_quantities, data_sample,
-                                       time_interval,
-                                       interpolation_degree)
-print(results)
-best_result = ParameterEstimation.filter_solutions(results, model, time_interval,
-                                                   data_sample)
+results = ParameterEstimation.estimate_over_degrees(model, measured_quantities, data_sample,
+                                                    time_interval)
