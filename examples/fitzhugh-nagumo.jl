be8fb1f1360b3f17f619cececc31412dc0693ee6
Ilia <i.ilmer@icloud.com>
2022-11-29 09:42:07 -0500

    update examples
    Signed-off-by: Ilia <i.ilmer@icloud.com>

--- examples/fitzhugh-nagumo.jl
+++ examples/fitzhugh-nagumo.jl
@@ -3,15 +3,15 @@ import ParameterEstimation
 using ModelingToolkit, Plots, Nemo, HomotopyContinuation, DifferentialEquations
 
 @parameters g a b
-@variables t V(t) R(t) y1(t)
+@variables t V(t) R(t) y1(t) y2(t)
 D = Differential(t)
 
 u0 = [1.0, -1.0]
 time_interval = (0.0, 1.2)
-datasize = 20
+datasize = 10
 tsteps = range(time_interval[1], time_interval[2], length = datasize)
 p_true = [2, 2 / 10, 2 / 10] # True Parameters
-measured_quantities = [y1 ~ V]
+measured_quantities = [y1 ~ V, y2 ~ R * V]
 states = [V, R]
 parameters = [g, a, b]
 
@@ -22,8 +22,7 @@ parameters = [g, a, b]
 
 prob_true = ODEProblem(model, u0, time_interval, p_true)
 solution_true = ModelingToolkit.solve(prob_true, Tsit5(), p = p_true, saveat = tsteps)
-data_sample = solution_true[1, :]
+data_sample = Dict(V => solution_true[V], V * R => solution_true[V * R])
 
-interpolation_degree = 7
 results = ParameterEstimation.estimate_over_degrees(model, measured_quantities, data_sample,
                                                     time_interval)
