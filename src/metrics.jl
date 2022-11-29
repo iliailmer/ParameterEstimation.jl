fe0b0d41ae8a8b72802de589cf7f0b8be6a7351d
Ilia <i.ilmer@icloud.com>
2022-11-29 09:43:38 -0500

    more verbose assertion
    Signed-off-by: Ilia <i.ilmer@icloud.com>

--- src/metrics.jl
+++ src/metrics.jl
@@ -1,4 +1,4 @@
 function mean_abs_err(y_true, y_sample)
-    @assert length(y_true)==length(y_sample) "Unequal sample sizes"
+    @assert length(y_true)==length(y_sample) "Unequal sample sizes: $(length(y_true)) and $(length(y_sample))"
     return sum(abs.(y_true .- y_sample)) / length(y_sample)
 end
