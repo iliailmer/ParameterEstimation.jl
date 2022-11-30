3eeeacf943addc5e8ba40583a296abc0e5038591
Ilia <i.ilmer@icloud.com>
2022-11-30 10:56:28 -0500

    small changes to the structure for counting
    Signed-off-by: Ilia <i.ilmer@icloud.com>

--- src/identifiability/identifiability_data.jl
+++ src/identifiability/identifiability_data.jl
@@ -3,16 +3,18 @@ struct IdentifiabilityData
     denomiantor::Nemo.fmpq_mpoly
     variables::Vector{Nemo.fmpq_mpoly}
     substitutions::Vector{Vector}
+    identifiability_nemo::Any
     identifiability::Dict{Any, Any}
     transcendence_basis_subs::Vector{Nemo.RingElem}
     Y_eq::Dict{Any, Any}
     basis::Vector{Nemo.fmpq_mpoly}
     weights::Dict{fmpq_mpoly, Int64}
     non_jet_ring::Nemo.FmpqMPolyRing
+    nemo_mtk::Dict
     function IdentifiabilityData(input::Dict)
         return new(input["polynomial_system"], input["denominator"], input["vars"],
-                   input["vals"],
-                   input["identifiability"], input["transcendence_basis_subs"],
-                   input["Y_eq"], input["basis"], input["weights"], input["non_jet_ring"])
+                   input["vals"], input["identifiability_nemo"], input["identifiability"],
+                   input["transcendence_basis_subs"], input["Y_eq"], input["basis"],
+                   input["weights"], input["non_jet_ring"], input["nemo_mtk"])
     end
 end
