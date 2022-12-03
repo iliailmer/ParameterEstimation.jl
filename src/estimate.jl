ec755a231aeb98ca261db60e81b4a7dbce34e1e6
Ilia <i.ilmer@icloud.com>
2022-12-03 16:58:46 -0500

    remove Result structure to separate file
    Signed-off-by: Ilia <i.ilmer@icloud.com>

--- src/estimate.jl
+++ src/estimate.jl
@@ -1,15 +1,3 @@
-struct Estimate
-    estimate::Any
-    err::Float64
-    return_code::Any
-end
-
-function Base.show(io::IO, e::Estimate)
-    println(io, "Estimate: ", e.estimate)
-    println(io, "Error: ", e.err)
-    println(io, "Return Code: ", e.return_code)
-end
-
 function estimate(model::ModelingToolkit.ODESystem,
                   measured_quantities::Vector{ModelingToolkit.Equation},
                   data_sample::Dict{Num, Vector{T}} = Dict{Num, Vector{T}}(),
@@ -35,10 +23,11 @@ function estimate(model::ModelingToolkit.ODESystem,
     if length(all_solutions) == 0
         all_solutions = HomotopyContinuation.solutions(results)
         if length(all_solutions) == 0
-            @warn "Interpolation degree $(interpolation_degree) No solutions found"
+            @warn "Interpolation degree $(interpolation_degree): No solutions found"
+            return []
         end
     end
-    all_solutions_dict = []
+    all_solutions_ = Vector{EstimationResult}([])
     state_str_map = Dict(replace(string(x), "(t)" => "") => x for x in states)
     param_str_map = Dict(string(x) => x for x in parameters)
     state_param_map = merge(state_str_map, param_str_map)
@@ -51,9 +40,11 @@ function estimate(model::ModelingToolkit.ODESystem,
                 tmp[state_param_map[string(v)[1:(end - 2)]]] = sol[idx]
             end
         end
-        push!(all_solutions_dict, tmp)
+        param_est = EstimationResult(model, tmp, interpolation_degree, ReturnCode.Success)
+        push!(all_solutions_, param_est)
     end
-    return all_solutions_dict
+
+    return all_solutions_
 end
 
 function estimate_over_degrees(model::ModelingToolkit.ODESystem,
@@ -78,25 +69,32 @@ function estimate_over_degrees(model::ModelingToolkit.ODESystem,
                                   time_interval,
                                   identifiability_result,
                                   deg)
-            filtered = filter_solutions(unfiltered, identifiability_result, model,
-                                        data_sample, time_interval)
-            push!(estimates, deg => filtered)
+            if length(unfiltered) > 0
+                filtered = filter_solutions(unfiltered, identifiability_result, model,
+                                            data_sample, time_interval)
+                push!(estimates, filtered)
+            else
+                push!(estimates,
+                      EstimationResult(model, Dict(), deg, ReturnCode.Failure))
+            end
         end
     end
     best_solution = nothing
-    for each in estimates
-        if each[2].return_code == ReturnCode.Success
-            if best_solution == nothing
-                best_solution = each
-            else
-                if each[2].err < best_solution[2].err
-                    best_solution = each
+    for est in estimates
+        if !(est isa Vector)
+            if est.return_code == ReturnCode.Success
+                if best_solution == nothing
+                    best_solution = est
+                else
+                    if est.err < best_solution.err
+                        best_solution = est
+                    end
                 end
             end
         end
     end
     if best_solution != nothing
-        @info "Best estimate $(best_solution[2].estimate) found at degree $(best_solution[1]) with error $(best_solution[2].err)"
+        @info "Best estimate found at degree $(best_solution.degree) with error $(best_solution.err)"
         return best_solution
     else
         @warn "No solution found"
@@ -104,49 +102,65 @@ function estimate_over_degrees(model::ModelingToolkit.ODESystem,
     end
 end
 
-function filter_solutions(results, identifiability_result::IdentifiabilityData,
+function solve_ode(model, estimate::EstimationResult, tsteps, data_sample; solver = Tsit5(),
+                   return_ode = false)
+    initial_conditions = [estimate[s] for s in ModelingToolkit.states(model)]
+    parameter_values = [estimate[p] for p in ModelingToolkit.parameters(model)]
+    prob = ModelingToolkit.ODEProblem(model, initial_conditions,
+                                      (tsteps[1], tsteps[end]),
+                                      parameter_values)
+    ode_solution = ModelingToolkit.solve(prob, solver, saveat = tsteps)
+    if ode_solution.retcode == ReturnCode.Success
+        err = 0
+        for (key, sample) in data_sample
+            err += ParameterEstimation.mean_abs_err(ode_solution[key], sample)
+        end
+        err /= length(data_sample)
+    else
+        err = 1e+10
+    end
+    if return_ode
+        return ode_solution,
+               EstimationResult(estimate.parameters, estimate.states,
+                                estimate.degree, err,
+                                estimate.return_code)
+    else
+        return EstimationResult(estimate.parameters, estimate.states,
+                                estimate.degree, err,
+                                estimate.return_code)
+    end
+end
+
+function solve_ode!(model, estimates::Vector{EstimationResult},
+                    tsteps, data_sample; solver = Tsit5())
+    estimates[:] = map(each -> solve_ode(model, each, tsteps, data_sample, solver = solver),
+                       estimates)
+end
+
+function filter_solutions(results::Vector{EstimationResult},
+                          identifiability_result::IdentifiabilityData,
                           model::ModelingToolkit.ODESystem,
                           data_sample::Dict{Num, Vector{T}} = Dict{Num, Vector{T}}(),
-                          time_interval = Vector{T}()) where {T <: Float}
+                          time_interval = Vector{T}(); topk = 1) where {T <: Float}
     @info "Filtering"
+    if all(each -> each.return_code == ReturnCode.Failure, results)
+        return results
+    end
     min_error = 1e+10
     best_estimate = nothing
     tsteps = range(time_interval[1], time_interval[2],
                    length = length(first(values(data_sample))))
     if length(identifiability_result["identifiability"]["nonidentifiable"]) > 0
         @warn "The model contains non-identifiable parameters, no filtering was done."
-        return Estimate(results, min_error, ReturnCode.Terminated)
+        return results
     end
-    @showprogress for (i, each_result) in enumerate(results)
-        initial_conditions = filter(x -> x != nothing,
-                                    [get(each_result, s, nothing)
-                                     for s in ModelingToolkit.states(model)])
-        parameter_values = filter(x -> x != nothing,
-                                  [get(each_result, p, nothing)
-                                   for p in ModelingToolkit.parameters(model)])
-        prob = ModelingToolkit.ODEProblem(model, initial_conditions, time_interval,
-                                          parameter_values)
-        ode_solution = ModelingToolkit.solve(prob, Tsit5(), saveat = tsteps)
-        if ode_solution.retcode == ReturnCode.Success
-            err = 0
-            for (key, sample) in data_sample
-                err += ParameterEstimation.mean_abs_err(ode_solution[key], sample)
-            end
-            err /= length(data_sample)
-            @info "\tSolution $i, error $err"
-        else
-            err = 1e+10
-            @info "\tSolution $i, error ∞, probably unstable, return code: $(ode_solution.retcode)"
-        end
-        if err < min_error
-            min_error = err
-            best_estimate = each_result
-        end
-    end
-    if best_estimate == nothing
-        @warn "Could not find a good estimate."
+    solve_ode!(model, results, tsteps, data_sample)
+    sorted = sort(results, by = x -> x.err)
+    if topk == 1
+        @info "Best estimate yelds ODE solution error $(sorted[1].err)"
+        return sorted[1]
     else
-        @info "Best estimate: $(best_estimate), error: $(min_error)"
+        @info "Best $(topk) estimates yeld ODE solution errors $([s.err for s in sorted[1:topk]])"
+        return sorted[1:topk]
     end
-    return Estimate(best_estimate, min_error, ReturnCode.Success)
 end
