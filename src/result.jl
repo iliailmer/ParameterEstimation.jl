35ec27198d4582e31862cfb36d02c7c9e996cd79
Ilia <i.ilmer@icloud.com>
2022-12-03 16:58:53 -0500

    result structure
    Signed-off-by: Ilia <i.ilmer@icloud.com>

---
+++ src/result.jl
@@ -0,0 +1,44 @@
+struct EstimationResult
+    parameters::OrderedDict
+    states::OrderedDict
+    degree::Int
+    err::Union{Nothing, Float64}
+    return_code::Any
+    function EstimationResult(model, poly_sol, degree, return_code)
+        parameters = OrderedDict{Any, Any}()
+        states = OrderedDict{Any, Any}()
+        for p in ModelingToolkit.parameters(model)
+            parameters[p] = get(poly_sol, p, nothing)
+        end
+        for s in ModelingToolkit.states(model)
+            states[s] = get(poly_sol, s, nothing)
+        end
+        new(parameters, states, degree, nothing, return_code)
+    end
+    function EstimationResult(parameters, states, degree, err, return_code)
+        new(parameters, states, degree, err, return_code)
+    end
+end
+
+Base.get(sol::EstimationResult, k) = get(sol.parameters, k, get(sol.states, k, nothing))
+
+function Base.getindex(sol::EstimationResult, k)
+    if haskey(sol.parameters, k)
+        return sol.parameters[k]
+    elseif haskey(sol.states, k)
+        return sol.states[k]
+    else
+        throw(KeyError(k))
+    end
+end
+
+function Base.show(io::IO, e::EstimationResult)
+    println(io, "Parameter Estimates:\n\t",
+            join([@sprintf("%s = %.6f", k, v) for (k, v) in pairs(e.parameters)],
+                 ", "))
+    println(io, "Initial Condition Estimates:\n\t",
+            join([@sprintf("%s = %.6f", k, v) for (k, v) in pairs(e.states)], ", "))
+    println(io, "Degree: ", e.degree)
+    println(io, "Error: ", @sprintf("%.4e", e.err))
+    println(io, "Return Code: ", e.return_code)
+end
