54a68cbf4a311cf2952fbf0059cdba17c1d6ccfc
Ilia <i.ilmer@icloud.com>
2022-12-11 19:23:41 -0500

    adding benchmarks and examples
    Signed-off-by: Ilia <i.ilmer@icloud.com>

---
+++ examples/all-global/simple2.jl
@@ -0,0 +1,37 @@
+using ModelingToolkit, DifferentialEquations, Plots
+
+using ParameterEstimation
+
+@parameters a b
+@variables t x1(t) x2(t) y1(t) y2(t)
+D = Differential(t)
+states = [x1, x2]
+parameters = [a, b]
+
+@named model = ODESystem([
+                             D(x1) ~ -a * x2,
+                             D(x2) ~ 1 / b * (x1),
+                         ], t, states, parameters)
+measured_quantities = [
+    y1 ~ x1,
+    y2 ~ x2,
+]
+
+ic = [1.0, 1.0]
+time_interval = (0.0, 2.0 * pi * sqrt(1.3 / 9.8))
+datasize = 10
+data_sample = ParameterEstimation.sample_data(model, measured_quantities, time_interval,
+                                              p_true, ic,
+                                              datasize; solver = solver)
+plot(solution_true)
+identifiability_result = ParameterEstimation.check_identifiability(model;
+                                                                   measured_quantities = measured_quantities)
+interpolation_degree = 7
+res = ParameterEstimation.estimate(model, measured_quantities, data_sample,
+                                   time_interval, identifiability_result,
+                                   interpolation_degree)
+filtered = ParameterEstimation.filter_solutions(res, identifiability_result, model,
+                                                data_sample, time_interval)
+
+res = ParameterEstimation.estimate_over_degrees(model, measured_quantities, data_sample,
+                                                time_interval)
