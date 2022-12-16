89e8698f6b912e8772ca404757f009af539d7912
Ilia <i.ilmer@icloud.com>
2022-12-15 21:10:19 -0500

    add solver + tol
    Signed-off-by: Ilia <i.ilmer@icloud.com>

--- src/utils.jl
+++ src/utils.jl
@@ -89,10 +89,12 @@ function sample_data(model::ModelingToolkit.ODESystem,
                      time_interval::Vector{T},
                      p_true::Vector{T},
                      u0::Vector{T},
-                     num_points::Int; solver = AutoTsit5(Rosenbrock23())) where {T <: Float}
+                     num_points::Int;
+                     solver = Tsit5()) where {T <: Float}
     tsteps = range(time_interval[1], time_interval[2], length = num_points)
     problem = ODEProblem(model, u0, time_interval, p_true)
-    solution_true = ModelingToolkit.solve(problem, solver, p = p_true, saveat = tsteps)
+    solution_true = ModelingToolkit.solve(problem, solver, p = p_true, saveat = tsteps;
+                                          abstol = 1e-10, reltol = 1e-10)
     data_sample = Dict(Num(v.rhs) => solution_true[Num(v.rhs)] for v in measured_data)
     return data_sample
 end
