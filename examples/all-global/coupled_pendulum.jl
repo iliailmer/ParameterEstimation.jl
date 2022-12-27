bab116e0bc4747aa87a54acc6a9c7a35b1076600
Ilia <i.ilmer@icloud.com>
2022-12-27 10:30:18 -0500

    check
    Signed-off-by: Ilia <i.ilmer@icloud.com>

--- examples/all-global/coupled_pendulum.jl
+++ examples/all-global/coupled_pendulum.jl
@@ -20,13 +20,12 @@ parameters = [alpha]
 #initial conditions
 ic = [1.0, 0.0, 0.0, -1.0]
 time_interval = [0.0, 10.0]
-datasize = 100
+datasize = 32
 p_true = [1.0]
 measured_quantities = [y1 ~ theta_1, y2 ~ theta_2 + theta_1]
 data_sample = ParameterEstimation.sample_data(model, measured_quantities, time_interval,
                                               p_true, ic, datasize; solver = solver)
-# identifiability_result = ParameterEstimation.check_identifiability(model;
-#                                                                    measured_quantities = measured_quantities)
+
 res = ParameterEstimation.estimate_over_degrees(model, measured_quantities, data_sample,
                                                 time_interval; solver = solver)
 print(res)
