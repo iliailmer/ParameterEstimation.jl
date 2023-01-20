0ef3c830a920d2bb570f5f967c2509901be82e60
Ilia <i.ilmer@icloud.com>
2023-01-20 12:50:26 -0500

    [skip ci] edit a couple examples for uneven time sampling
    Signed-off-by: Ilia <i.ilmer@icloud.com>

--- examples/all-global/paper-ex.jl
+++ examples/all-global/paper-ex.jl
@@ -10,8 +10,13 @@ D = Differential(t)
 # data sampling
 outs = [y1 ~ x1 + x1^2]
 time = [0.0, 1.0] # sampling interval
-data = Dict(x1 + x1^2 => [2.0, 1.56301,
-                1.22995, 0.97441])
+data = Dict{Any, Vector{Float64}}("t" => [0.0, 1 / 3, 2 / 3, 1.0],
+                                  x1 + x1^2 => [2.0, 1.56301,
+                                      1.22995, 0.97441])
 # identifiabliity,
 # interpolation and estimation (steps 1-5)
 res = estimate(model, outs, data, time)
+identifiability_result = ParameterEstimation.check_identifiability(model;
+                                                                   measured_quantities = outs)
+res = ParameterEstimation.estimate_fixed_degree(model, outs, data, time,
+                                                identifiability_result, 1)
