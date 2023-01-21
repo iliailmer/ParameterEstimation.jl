1d61e177f95d35d1397140fd93515dd0a5ca26a5
Ilia <i.ilmer@icloud.com>
2023-01-21 09:40:31 -0500

    expand paper ex
    Signed-off-by: Ilia <i.ilmer@icloud.com>

--- examples/all-global/paper-ex.jl
+++ examples/all-global/paper-ex.jl
@@ -16,4 +16,12 @@ data = Dict("t" => [0.000, 0.333, 0.666, 1.000],
             x + x^2 => [2.000, 1.563, 1.229, 0.974])
 
 # Run
-res = ParameterEstimation.estimate(Σ, outs, data)
+res = estimate(Σ, outs, data)
+
+identifiability_result = ParameterEstimation.check_identifiability(Σ;
+                                                                   measured_quantities = outs)
+interpolation_degree = 2
+res = ParameterEstimation.estimate_fixed_degree(Σ, outs, data,
+                                                identifiability_result,
+                                                interpolation_degree)
+ParameterEstimation.filter_solutions(res, identifiability_result, Σ, data)
