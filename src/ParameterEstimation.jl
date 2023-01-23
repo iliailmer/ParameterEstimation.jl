f57fa84d26ecb8c8a3d48a45652feca62b6fce56
Ilia <i.ilmer@icloud.com>
2023-01-23 15:17:40 -0500

    includes.jl

--- src/ParameterEstimation.jl
+++ src/ParameterEstimation.jl
@@ -1,28 +1,15 @@
 module ParameterEstimation
 import DifferentialEquations: Tsit5
 import TaylorSeries: Taylor1
+import OrderedCollections: OrderedDict
 
 using ProgressMeter, Logging, Printf
 using ModelingToolkit, LinearSolve, LinearAlgebra
-using SIAN, HomotopyContinuation, Groebner
+using SIAN, HomotopyContinuation, Groebner, Oscar
 using .ReturnCode
 
 Float = Union{Float64, Float32, Float16}
-
-include("rational_interpolation/rational_interpolation.jl")
-include("rational_interpolation/interpolant.jl")
-
-include("identifiability/check_identifiability.jl")
-include("identifiability/transcendence_basis.jl")
-include("identifiability/identifiability_data.jl")
-include("identifiability/utils.jl")
-
-include("result.jl")
-include("estimate.jl")
-include("filtering.jl")
-
-include("utils.jl")
-include("metrics.jl")
+include("includes.jl")
 
 export check_identifiability, estimate, filter_solutions, sample_data
 
