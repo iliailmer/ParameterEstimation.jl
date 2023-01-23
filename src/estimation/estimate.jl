b7c619be93b1d201d98e2afe5725cb38b1168f48
Ilia <i.ilmer@icloud.com>
2023-01-23 16:13:34 -0500

    kwargs and solution estimates

--- src/estimation/estimate.jl
+++ src/estimation/estimate.jl
@@ -11,26 +11,25 @@ Run estimation over a range of interpolation degrees. Return the best estimate a
 """
 function estimate(model::ModelingToolkit.ODESystem,
                   measured_quantities::Vector{ModelingToolkit.Equation},
-                  data_sample::Dict{Any, Vector{T}} = Dict{Any, Vector{T}}(),
-                  at_time::T = 0.0; method = :homotopy, solver = Tsit5(),
+                  data_sample::Dict{Any, Vector{T}} = Dict{Any, Vector{T}}();
+                  at_time::T = 0.0, method = :homotopy, solver = Tsit5(),
                   degree_range = nothing, real_tol = 1e-10,
                   threaded = Threads.nthreads() > 1) where {T <: Float}
     if !(method in [:homotopy, :msolve])
         throw(ArgumentError("Method $method is not supported, must be one of :homotopy or :msolve."))
     end
     if threaded
-        result = estimate_threaded(model, measured_quantities, data_sample,
-                                   at_time; solver = solver,
+        result = estimate_threaded(model, measured_quantities, data_sample;
+                                   at_time = at_time, solver = solver,
                                    degree_range = degree_range,
                                    method = method,
                                    real_tol = real_tol)
         display(result)
         return result
     else
-        result = estimate_serial(model, measured_quantities, data_sample,
-                                 at_time; solver = solver,
-                                 degree_range = degree_range,
-                                 method = method,
+        result = estimate_serial(model, measured_quantities, data_sample;
+                                 solver = solver, at_time = at_time,
+                                 degree_range = degree_range, method = method,
                                  real_tol = real_tol)
         display(result)
         return result
--- src/estimation/solve.jl
+++ src/estimation/solve.jl
@@ -1,9 +1,10 @@
 function solve_via_homotopy(identifiability_result, model; real_tol = 1e-10)
-    parameters = ModelingToolkit.parameters(model)
-    polynomial_system = identifiability_result["polynomial_system_to_solve"]
+    @debug "Solving $(length(identifiability_result["polynomial_system_to_solve"])) polynomial equations in $(length(identifiability_result["polynomial_system_to_solve"].variables)) variables"
 
-    @info "Solving $(length(polynomial_system)) polynomial equations in $(length(polynomial_system.variables)) variables"
-    states = ModelingToolkit.states(model)
+    polynomial_system = identifiability_result["polynomial_system_to_solve"]
+    state_param_map = merge(Dict(replace(string(x), "(t)" => "") => x
+                                 for x in ModelingToolkit.states(model)),
+                            Dict(string(x) => x for x in ModelingToolkit.parameters(model)))
     results = HomotopyContinuation.solve(polynomial_system; show_progress = false)
     all_solutions = HomotopyContinuation.real_solutions(results)
     if length(all_solutions) == 0
@@ -14,8 +15,6 @@ function solve_via_homotopy(identifiability_result, model; real_tol = 1e-10)
         end
     end
     all_solutions_ = Vector{Dict}([])
-    state_param_map = merge(Dict(replace(string(x), "(t)" => "") => x for x in states),
-                            Dict(string(x) => x for x in parameters))
     for sol in all_solutions
         tmp = Dict()
         sol = map(each -> to_exact(each; tol = real_tol), sol)
@@ -36,10 +35,31 @@ end
 
 function solve_via_msolve(identifiability_result, model; real_tol = 1e-10)
     polynomial_system = identifiability_result["polynomial_system_to_solve"]
+    state_param_map = merge(Dict(replace(string(x), "(t)" => "") => x
+                                 for x in ModelingToolkit.states(model)),
+                            Dict(string(x) => x for x in ModelingToolkit.parameters(model)))
+
     all_vars = reduce((x, y) -> union(x, y), vars(poly) for poly in polynomial_system)
-    R, _ = Oscar.PolynomialRing(QQ, string.(all_vars))
-    ps = [change_ring(poly, R) for poly in polynomial_system]
+    R, _ = Oscar.PolynomialRing(QQ, string.(all_vars); ordering = :degrevlex)
+    ps = [SIAN.parent_ring_change(poly, R) for poly in polynomial_system]
     i = Oscar.ideal(R, ps)
-    solution = Oscar.msolve(i)
-    throw(ArgumentError("Function solve_via_msolve is not implemented yet"))
+    all_solutions_ = Vector{Dict}([])
+
+    rat_param, solutions = Oscar.msolve(i)
+    for sol in solutions
+        tmp = Dict()
+        sol = map(each -> Float64(each), sol)
+        for (idx, v) in enumerate(all_vars)
+            if endswith(string(v), "_0")
+                tmp[state_param_map[string(v)[1:(end - 2)]]] = sol[idx]
+            end
+        end
+        for (key, val) in identifiability_result.transcendence_basis_subs
+            if endswith(string(key), "_0")
+                tmp[state_param_map[string(key)[1:(end - 2)]]] = Int(Meta.parse("$val"))
+            end
+        end
+        push!(all_solutions_, tmp)
+    end
+    return all_solutions_
 end
