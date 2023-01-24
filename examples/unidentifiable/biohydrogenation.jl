3d0bd310d38b4826a179fd3c6e64ad0c32bf2df9
Ilia <i.ilmer@icloud.com>
2023-01-24 14:05:39 -0500

    [skip ci] method in biohydrogenation
    Signed-off-by: Ilia <i.ilmer@icloud.com>

--- examples/unidentifiable/biohydrogenation.jl
+++ examples/unidentifiable/biohydrogenation.jl
@@ -28,5 +28,5 @@ p_true = [1, 1.3, 1.1, 1.2, 1.1, 1] # True Parameters
 data_sample = ParameterEstimation.sample_data(model, measured_quantities, time_interval,
                                               p_true, ic, datasize; solver = solver)
 
-res = ParameterEstimation.estimate(model, measured_quantities, data_sample,
-                                   time_interval)
+res = ParameterEstimation.estimate(model, measured_quantities, data_sample;
+                                   method = :msolve)
